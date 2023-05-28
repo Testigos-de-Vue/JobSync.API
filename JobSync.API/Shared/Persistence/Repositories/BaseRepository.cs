a9d2cc534ce83df4f5f3085ad3427077537d783a
aleiva <aleiva1700@gmail.com>
2023-05-27 21:00:33 -0500

    chore: create base repository type

---
+++ JobSync.API/Shared/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobSync.API.Shared.Persistence.Contexts;
+
+namespace JobSync.API.Shared.Persistence.Repositories;
+
+public class BaseRepository
+{
+  protected readonly AppDbContext Context;
+
+  public BaseRepository(AppDbContext context)
+  {
+    Context = context;
+  }
+}
