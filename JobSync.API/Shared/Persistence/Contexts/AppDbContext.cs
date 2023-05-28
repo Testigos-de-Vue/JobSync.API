e6f956beeaed408c79ca2c161738471cc8587fc7
aleiva <aleiva1700@gmail.com>
2023-05-27 20:29:24 -0500

    chore: add object relational mapping rules

---
+++ JobSync.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using JobSync.API.Shared.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobSync.API.Shared.Persistence.Contexts;
+
+public class AppDbContext : DbContext
+{
+
+  public AppDbContext(DbContextOptions options) : base(options)
+  {
+  }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+    
+    // Apply Snake Case Naming Convention
+    builder.UseSnakeCaseNamingConvention();
+  }
+}
