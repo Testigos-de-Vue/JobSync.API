1dfb5b12056af3cd64d45718f85ce04ed25c44d9
aleiva <aleiva1700@gmail.com>
2023-05-29 11:50:44 -0500

    feat: create user service contract

---
+++ JobSync.API/Authentication/Domain/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using JobSync.API.Authentication.Domain.Models;
+using JobSync.API.Authentication.Domain.Services.Communication;
+
+namespace JobSync.API.Authentication.Domain.Services;
+
+public interface IUserService
+{
+  Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
+  Task<IEnumerable<User>> ListAsync();
+  Task<UserResponse> RegisterAsync(RegisterRequest request);
+  Task<UserResponse> UpdateAsync(int id, User user);
+  Task<UserResponse> DeleteAsync(int id);
+}
