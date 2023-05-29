8c8f6f98f64bc1356e42a048d98407408de1c251
aleiva <aleiva1700@gmail.com>
2023-05-28 22:44:01 -0500

    chore: implement user relational mapping rules

--- JobSync.API/Shared/Persistence/Contexts/AppDbContext.cs
+++ JobSync.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -1,11 +1,13 @@
-﻿using JobSync.API.Shared.Extensions;
+﻿using JobSync.API.Authentication.Domain.Models;
+using JobSync.API.Shared.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobSync.API.Shared.Persistence.Contexts;
 
 public class AppDbContext : DbContext
 {
-
+  public DbSet<User> Users { get; set; }
+  
   public AppDbContext(DbContextOptions options) : base(options)
   {
   }
@@ -13,6 +15,17 @@ public class AppDbContext : DbContext
   protected override void OnModelCreating(ModelBuilder builder)
   {
     base.OnModelCreating(builder);
+    
+    // Users Configuration
+
+    builder.Entity<User>().ToTable("Users");
+    builder.Entity<User>().HasKey(u => u.Id);
+    builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+    builder.Entity<User>().Property(u => u.Name).IsRequired().HasMaxLength(32);
+    builder.Entity<User>().Property(u => u.LastName).IsRequired().HasMaxLength(64);
+    builder.Entity<User>().Property(u => u.Email).IsRequired().HasMaxLength(64);
+    builder.Entity<User>().Property(u => u.ImageUrl).IsRequired().HasMaxLength(256);
+    builder.Entity<User>().Property(u => u.Password).IsRequired();
     
     // Apply Snake Case Naming Convention
     builder.UseSnakeCaseNamingConvention();
