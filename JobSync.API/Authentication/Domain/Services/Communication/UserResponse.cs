51c93b2a2ec6a34d92d479b8a0b02a238e91d8c4
aleiva <aleiva1700@gmail.com>
2023-05-28 23:10:15 -0500

    feat: add user response type

---
+++ JobSync.API/Authentication/Domain/Services/Communication/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using JobSync.API.Authentication.Domain.Models;
+using JobSync.API.Shared.Domain.Services.Communication;
+
+namespace JobSync.API.Authentication.Domain.Services.Communication;
+
+public class UserResponse : BaseResponse<User>
+{
+  public UserResponse(string message) : base(message)
+  {
+  }
+
+  public UserResponse(User resource) : base(resource)
+  {
+  }
+}
