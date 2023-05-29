d3b9922c35f979df8041ca897ca758220a8bb084
aleiva <aleiva1700@gmail.com>
2023-05-29 09:59:46 -0500

    feat: add authenticate request and response types

---
+++ JobSync.API/Authentication/Domain/Services/Communication/AuthenticateResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace JobSync.API.Authentication.Domain.Services.Communication;
+
+public class AuthenticateResponse
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string LastName { get; set; }
+  public string Email { get; set; }
+  public string ImageUrl { get; set; }
+  public string Token { get; set; }
+}
