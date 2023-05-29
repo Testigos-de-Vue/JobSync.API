0b35620b5ce1f3152c2b97d27c5ebae74f7732a6
aleiva <aleiva1700@gmail.com>
2023-05-28 21:57:54 -0500

    feat: add user entity

---
+++ JobSync.API/Authentication/Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JobSync.API.Authentication.Domain.Models;
+
+public class User
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string LastName { get; set; }
+  public string Email { get; set; }
+  public string ImageUrl { get; set; }
+  
+  [JsonIgnore]
+  public string Password { get; set; }
+}
