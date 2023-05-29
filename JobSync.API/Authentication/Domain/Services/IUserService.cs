6dea3ac6e28eddb3fcc9d6e73e0fd896653ff00b
aleiva <aleiva1700@gmail.com>
2023-05-29 12:15:57 -0500

    refactor: remove unnecessary and wrong functions from user service contract

--- JobSync.API/Authentication/Domain/Services/IUserService.cs
+++ JobSync.API/Authentication/Domain/Services/IUserService.cs
@@ -5,9 +5,8 @@ namespace JobSync.API.Authentication.Domain.Services;
 
 public interface IUserService
 {
-  Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
   Task<IEnumerable<User>> ListAsync();
-  Task<UserResponse> CreateAsync(RegisterRequest request);
+  Task<UserResponse> CreateAsync(User user);
   Task<UserResponse> UpdateAsync(int id, User user);
   Task<UserResponse> DeleteAsync(int id);
 }
