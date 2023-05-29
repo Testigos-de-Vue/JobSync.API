ec43076ac2caf24a68264b08e3be9915d28361ef
aleiva <aleiva1700@gmail.com>
2023-05-28 23:20:34 -0500

    feat: add password hashing service contract

---
+++ JobSync.API/Authentication/Domain/Services/IPasswordHashingService.cs
@@ -0,0 +1,7 @@
+﻿namespace JobSync.API.Authentication.Domain.Services;
+
+public interface IPasswordHashingService
+{
+  string GetHash(string password);
+  bool VerifyPassword(string password, string hashedPassword);
+}
