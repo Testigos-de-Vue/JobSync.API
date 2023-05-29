23bbf5eaa4204b36f01b184bc27489edcae4a87d
aleiva <aleiva1700@gmail.com>
2023-05-29 11:54:25 -0500

    refactor: rename user register async function to improve clarity

--- JobSync.API/Authentication/Domain/Services/IUserService.cs
+++ JobSync.API/Authentication/Domain/Services/IUserService.cs
@@ -7,7 +7,7 @@ public interface IUserService
 {
   Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
   Task<IEnumerable<User>> ListAsync();
-  Task<UserResponse> RegisterAsync(RegisterRequest request);
+  Task<UserResponse> CreateAsync(RegisterRequest request);
   Task<UserResponse> UpdateAsync(int id, User user);
   Task<UserResponse> DeleteAsync(int id);
 }
