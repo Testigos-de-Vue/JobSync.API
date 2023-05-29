6b29d78c1cbebf6953f46858a5f866ac839e8d19
aleiva <aleiva1700@gmail.com>
2023-05-29 09:13:29 -0500

    feat: implement password hashing service

---
+++ JobSync.API/Authentication/Services/PasswordHashingService.cs
@@ -0,0 +1,16 @@
+﻿using JobSync.API.Authentication.Domain.Services;
+
+namespace JobSync.API.Authentication.Services;
+
+public class PasswordHashingService : IPasswordHashingService
+{
+  public string GetHash(string password)
+  {
+    return BCrypt.Net.BCrypt.HashPassword(password);
+  }
+
+  public bool VerifyPassword(string password, string hashedPassword)
+  {
+    return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+  }
+}
