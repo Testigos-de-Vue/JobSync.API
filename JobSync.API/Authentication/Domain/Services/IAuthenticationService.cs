4f05ecbb7bb6ca82e99d33b8273d0dd993d28916
aleiva <aleiva1700@gmail.com>
2023-05-29 22:22:58 -0500

    feat: create authentication service contract

---
+++ JobSync.API/Authentication/Domain/Services/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿using JobSync.API.Authentication.Domain.Services.Communication;
+
+namespace JobSync.API.Authentication.Domain.Services;
+
+public interface IAuthenticationService
+{
+  Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
+}
