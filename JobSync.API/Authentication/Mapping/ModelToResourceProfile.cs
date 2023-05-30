de28af1c8c9a9e95e91106bdaa0e34dd95b946fe
aleiva <aleiva1700@gmail.com>
2023-05-29 22:42:38 -0500

    chore: add object mapping rules for authentication bounded context

---
+++ JobSync.API/Authentication/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using JobSync.API.Authentication.Domain.Models;
+using JobSync.API.Authentication.Resources;
+
+namespace JobSync.API.Authentication.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+  public ModelToResourceProfile()
+  {
+    CreateMap<User, UserResource>();
+  }
+}
