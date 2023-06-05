5073bdf5e3f8099065fcbcced10485f60fe8519d
sayufu <lilifuye72@gmail.com>
2023-06-05 17:45:43 -0500

    feat: add data transfer object for process resources

--- JobSync.API/Recruitment/Resources/ProcessResource.cs
+++ JobSync.API/Recruitment/Resources/ProcessResource.cs
@@ -1,20 +1,13 @@
 ﻿using System.Runtime.InteropServices.JavaScript;
 
-namespace JobSync.API.Recruitment.Domain.Models;
+namespace JobSync.API.Recruitment.Resources;
 
-public class Process
+public class ProcessResource
 {
-    public Process()
-    {
-        Phases = new List<Phase>();
-    }
-
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public JSType.Date StartingDate { get; set; }
     public JSType.Date EndingDate { get; set; }
     public Boolean Status { get; set; }
-    
-    public List<Phase> Phases { get; set; }
 }
---
+++ JobSync.API/Recruitment/Resources/SaveProcessResource.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace JobSync.API.Recruitment.Resources;
+
+public class SaveProcessResource
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    
+    [MaxLength(128)]
+    public string Description { get; set; }
+    
+    [Required]
+    public JSType.Date StartingDate { get; set; }
+    
+    [Required]
+    public JSType.Date EndingDate { get; set; }
+    
+    public Boolean Status { get; set; }
+}
