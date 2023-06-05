7dbf1605f5e1f2c558f78c520c516446e73bbc48
sayufu <lilifuye72@gmail.com>
2023-06-05 17:45:54 -0500

    feat: add data transfer object for job area resources

---
+++ JobSync.API/Recruitment/Resources/JobAreaResource.cs
@@ -0,0 +1,7 @@
+﻿namespace JobSync.API.Recruitment.Resources;
+
+public class JobAreaResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ JobSync.API/Recruitment/Resources/SaveJobAreaResource.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobSync.API.Recruitment.Resources;
+
+public class SaveJobAreaResource
+{
+    [Required]
+    public string Name { get; set; }
+}
