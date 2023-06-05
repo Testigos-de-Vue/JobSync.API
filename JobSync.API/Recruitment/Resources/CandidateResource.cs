bfc9130e8d80133f19f394bb5a55729bb8a62397
sayufu <lilifuye72@gmail.com>
2023-06-05 17:44:41 -0500

    feat: add data transfer object for candidate resources

---
+++ JobSync.API/Recruitment/Resources/CandidateResource.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using JobSync.API.Authentication.Resources;
+
+namespace JobSync.API.Recruitment.Resources;
+
+public class CandidateResource
+{
+    public int Id { get; set; }
+    public string CvUrl { get; set; }
+    public Boolean IsActive { get; set; }
+    public JSType.Date PostulationDate { get; set; }
+
+    public UserResource UserArea { get; set; }
+    public JobAreaResource JobArea { get; set; }
+}
---
+++ JobSync.API/Recruitment/Resources/SaveCandidateResource.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace JobSync.API.Recruitment.Resources;
+
+public class SaveCandidateResource
+{
+    public string CvUrl { get; set; }
+    public Boolean IsActive { get; set; }
+    
+    [Required]
+    public JSType.Date PostulationDate { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+    
+    [Required]
+    public int JobAreaId { get; set; }
+}
