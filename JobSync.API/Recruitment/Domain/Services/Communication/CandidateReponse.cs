3b5642216eef752599c6727c0febe9704ae7695e
sayufu <lilifuye72@gmail.com>
2023-06-05 14:21:56 -0500

    feat: add communication and entities response

---
+++ JobSync.API/Recruitment/Domain/Services/Communication/CandidateReponse.cs
@@ -0,0 +1,15 @@
+﻿using JobSync.API.Recruitment.Domain.Models;
+using JobSync.API.Shared.Domain.Services.Communication;
+
+namespace JobSync.API.Recruitment.Domain.Services.Communication;
+
+public class CandidateReponse: BaseResponse<Candidate>
+{
+    public CandidateReponse(string message) : base(message)
+    {
+    }
+
+    public CandidateReponse(Candidate resource) : base(resource)
+    {
+    }
+}
---
+++ JobSync.API/Recruitment/Domain/Services/Communication/JobAreaResponse.cs
@@ -0,0 +1,15 @@
+﻿using JobSync.API.Recruitment.Domain.Models;
+using JobSync.API.Shared.Domain.Services.Communication;
+
+namespace JobSync.API.Recruitment.Domain.Services.Communication;
+
+public class JobAreaResponse: BaseResponse<JobArea>
+{
+    public JobAreaResponse(string message) : base(message)
+    {
+    }
+
+    public JobAreaResponse(JobArea resource) : base(resource)
+    {
+    }
+}
