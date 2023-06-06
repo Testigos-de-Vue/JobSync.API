92dfeaabebf14b107eff0aa74540788b310ab6f3
sayufu <lilifuye72@gmail.com>
2023-06-06 01:10:06 -0500

    fix: refactor of file names of candidate profile entity

---
+++ JobSync.API/Recruitment/Domain/Repositories/ICandidateProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobSync.API.Recruitment.Domain.Models;
+
+namespace JobSync.API.Recruitment.Domain.Repositories;
+
+public interface ICandidateProfileRepository
+{
+    Task<IEnumerable<CandidateProfile>>ListAsync();
+    Task AddAsync(CandidateProfile candidateProfile);
+    Task<CandidateProfile> FindByIdAsync(int candidateId);
+    Task<CandidateProfile> FindByJobAreaAsync(int jobAreaId);
+    void Update(CandidateProfile candidateProfile);
+    void Remove(CandidateProfile candidateProfile);
+}
---
+++ JobSync.API/Recruitment/Domain/Services/ICandidateProfileService.cs
@@ -0,0 +1,12 @@
+﻿using JobSync.API.Recruitment.Domain.Models;
+using JobSync.API.Recruitment.Domain.Services.Communication;
+
+namespace JobSync.API.Recruitment.Domain.Services;
+
+public interface ICandidateProfileService
+{
+    Task<IEnumerable<CandidateProfile>> ListAsync();
+    Task<CandidateProfileReponse>SaveAsync(CandidateProfile candidateProfile);
+    Task<CandidateProfileReponse> UpdateAsync(int id, CandidateProfile candidateProfile);
+    Task<CandidateProfileReponse> DeleteAsync(int id);
+}
