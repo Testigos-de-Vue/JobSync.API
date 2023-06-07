7c205327134e39dbca76fd95825a0960d62901a0
aleiva <aleiva1700@gmail.com>
2023-06-07 09:41:33 -0500

    feat: add incoming and outgoing transfer objects for role resource

---
+++ JobSync.API/Profile/Resources/RoleResource.cs
@@ -0,0 +1,7 @@
+﻿namespace JobSync.API.Profile.Resources;
+
+public class RoleResource
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+}
---
+++ JobSync.API/Profile/Resources/SaveRoleResource.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobSync.API.Profile.Resources;
+
+public class SaveRoleResource
+{
+  [Required]
+  public string Name { get; set; }
+}
