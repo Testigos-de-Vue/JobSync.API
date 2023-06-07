c101477385364de59bc133a502df1db147a35efa
aleiva <aleiva1700@gmail.com>
2023-06-07 12:31:12 -0500

    feat: add profile entity

---
+++ JobSync.API/Profile/Domain/Models/Profile.cs
@@ -0,0 +1,12 @@
+﻿namespace JobSync.API.Profile.Domain.Models;
+
+public class Profile
+{
+  public int Id { get; set; }
+  public string CvUrl { get; set; }
+  
+  // Relationships
+  public Role role;
+  public int OrganizationId { get; set; }
+  public int UserId { get; set; }
+}
