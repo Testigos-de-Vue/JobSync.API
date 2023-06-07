d183d4a9c139ecae71b05c91e4a195b276582264
aleiva <aleiva1700@gmail.com>
2023-06-07 09:49:22 -0500

    chore: add role object mapping rules for profile bounded context

---
+++ JobSync.API/Profile/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,12 @@
+﻿using JobSync.API.Profile.Domain.Models;
+using JobSync.API.Profile.Resources;
+
+namespace JobSync.API.Profile.Mapping;
+
+public class ResourceToModelProfile : AutoMapper.Profile
+{
+  public ResourceToModelProfile()
+  {
+    CreateMap<SaveRoleResource, Role>();
+  }
+}
