aecac7cf932b95cf4db3846cbbf34738de7735e7
aleiva <aleiva1700@gmail.com>
2023-06-07 13:39:23 -0500

    feat: add profile repository contract

---
+++ JobSync.API/Profile/Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace JobSync.API.Profile.Domain.Repositories;
+
+public interface IProfileRepository
+{
+  Task<IEnumerable<Models.Profile>> ListAsync();
+  Task AddAsync(Models.Profile profile);
+  Task<Models.Profile> FindByIdAsync(int id);
+  void Update(Models.Profile profile);
+  void Remove(Models.Profile profile);
+}
