758210c3c49b5a2a960d34457f4240805176b980
aleiva <aleiva1700@gmail.com>
2023-06-07 09:37:38 -0500

    feat: create role service contract

---
+++ JobSync.API/Profile/Domain/Services/IRoleService.cs
@@ -0,0 +1,12 @@
+﻿using JobSync.API.Profile.Domain.Models;
+using JobSync.API.Profile.Domain.Services.Communication;
+
+namespace JobSync.API.Profile.Domain.Services;
+
+public interface IRoleService
+{
+  Task<IEnumerable<Role>> ListAsync();
+  Task<RoleResponse> SaveAsync(Role role);
+  Task<RoleResponse> UpdateAsync(int roleId, Role role);
+  Task<RoleResponse> DeleteAsync(int roleId);
+}
