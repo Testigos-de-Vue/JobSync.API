88847a401031a0ae1c478fb4454dc8359496b32d
aleiva <aleiva1700@gmail.com>
2023-06-07 13:58:03 -0500

    feat: implement profile service contract

---
+++ JobSync.API/Profile/Domain/Services/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿using JobSync.API.Profile.Domain.Services.Communication;
+
+namespace JobSync.API.Profile.Domain.Services;
+
+public interface IProfileService
+{
+  Task<IEnumerable<Models.Profile>> ListAsync();
+  Task<ProfileResponse> SaveAsync(Models.Profile role);
+  Task<ProfileResponse> UpdateAsync(int roleId, Models.Profile role);
+  Task<ProfileResponse> DeleteAsync(int roleId);
+} 
