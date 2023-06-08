eeef69753f946e0b042c2a18dc46c6da17fadf5d
sayufu <lilifuye72@gmail.com>
2023-06-07 22:00:43 -0500

    fix: deleted candidate profile entity

--- JobSync.API/Shared/Persistence/Contexts/AppDbContext.cs
+++ JobSync.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -13,10 +13,8 @@ public class AppDbContext : DbContext
   public DbSet<Role> Roles { get; set; }
   public DbSet<JobSync.API.Profile.Domain.Models.Profile> Profiles { get; set; }
   public DbSet<TaskItem> TaskItems {get; set;}
-  public DbSet<JobArea> JobAreas { get; set; }
-  public DbSet<CandidateProfile> CandidateProfiles { get; set; }
-  public DbSet<RecruitmentPhase> Phases { get; set; }
-  public DbSet<RecruitmentProcess> Processes { get; set; }
+  public DbSet<Phase> Phases { get; set; }
+  public DbSet<Process> Processes { get; set; }
 
   public AppDbContext(DbContextOptions options) : base(options)
   {
@@ -56,38 +54,24 @@ public class AppDbContext : DbContext
     builder.Entity<TaskItem>().Property(t=>t.Id).IsRequired().ValueGeneratedOnAdd();
     builder.Entity<TaskItem>().Property(t=>t.Description).IsRequired().HasMaxLength(64);
     builder.Entity<TaskItem>().Property(t=>t.Date).IsRequired();
-    
-    // JobAreas Configuration
-    builder.Entity<JobArea>().ToTable("JobAreas");
-    builder.Entity<JobArea>().HasKey(j=>j.Id);
-    builder.Entity<JobArea>().Property(j=>j.Id).IsRequired().ValueGeneratedOnAdd();
-    builder.Entity<JobArea>().Property(j=>j.Name).IsRequired().HasMaxLength(64);
-    
-    // CandidateProfiles Configuration
-    builder.Entity<CandidateProfile>().ToTable("CandidateProfiles");
-    builder.Entity<CandidateProfile>().HasKey(c=>c.Id);
-    builder.Entity<CandidateProfile>().Property(c=>c.Id).IsRequired().ValueGeneratedOnAdd();
-    builder.Entity<CandidateProfile>().Property(c => c.CvUrl).IsRequired().HasMaxLength(256);
-    builder.Entity<CandidateProfile>().Property(c => c.IsActive).IsRequired();
-    builder.Entity<CandidateProfile>().Property(c => c.PostulationDate).IsRequired();
-    
+
     // RecruitmentPhases Configuration
-    builder.Entity<RecruitmentPhase>().ToTable("RecruitmentPhases");
-    builder.Entity<RecruitmentPhase>().HasKey(p=>p.Id);
-    builder.Entity<RecruitmentPhase>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
-    builder.Entity<RecruitmentPhase>().Property(p=>p.Name).IsRequired().HasMaxLength(64);
-    builder.Entity<RecruitmentPhase>().Property(p=>p.Description).IsRequired().HasMaxLength(256);
-    builder.Entity<RecruitmentPhase>().Property(p=>p.CreatedDate).IsRequired();
+    builder.Entity<Phase>().ToTable("RecruitmentPhases");
+    builder.Entity<Phase>().HasKey(p=>p.Id);
+    builder.Entity<Phase>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
+    builder.Entity<Phase>().Property(p=>p.Name).IsRequired().HasMaxLength(64);
+    builder.Entity<Phase>().Property(p=>p.Description).IsRequired().HasMaxLength(256);
+    builder.Entity<Phase>().Property(p=>p.CreatedDate).IsRequired();
     
     // RecruitmentProcesses Configuration
-    builder.Entity<RecruitmentProcess>().ToTable("RecruitmentProcesses");
-    builder.Entity<RecruitmentProcess>().HasKey(p=>p.Id);
-    builder.Entity<RecruitmentProcess>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
-    builder.Entity<RecruitmentProcess>().Property(p=>p.Name).IsRequired().HasMaxLength(64);
-    builder.Entity<RecruitmentProcess>().Property(p=>p.Description).IsRequired().HasMaxLength(256);
-    builder.Entity<RecruitmentProcess>().Property(p=>p.StartingDate).IsRequired();
-    builder.Entity<RecruitmentProcess>().Property(p=>p.EndingDate).IsRequired();
-    builder.Entity<RecruitmentProcess>().Property(p=>p.Status).IsRequired();
+    builder.Entity<Process>().ToTable("RecruitmentProcesses");
+    builder.Entity<Process>().HasKey(p=>p.Id);
+    builder.Entity<Process>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
+    builder.Entity<Process>().Property(p=>p.Name).IsRequired().HasMaxLength(64);
+    builder.Entity<Process>().Property(p=>p.Description).IsRequired().HasMaxLength(256);
+    builder.Entity<Process>().Property(p=>p.StartingDate).IsRequired();
+    builder.Entity<Process>().Property(p=>p.EndingDate).IsRequired();
+    builder.Entity<Process>().Property(p=>p.Status).IsRequired();
     
     // Relationships
     builder.Entity<JobSync.API.Profile.Domain.Models.Profile>()
@@ -100,20 +84,10 @@ public class AppDbContext : DbContext
       .WithOne(p => p.Role)
       .HasForeignKey(p => p.Id);
 
-    builder.Entity<RecruitmentProcess>()
+    builder.Entity<Process>()
       .HasMany(p => p.Phases)
-      .WithOne(p => p.RecruitmentProcess)
+      .WithOne(p => p.Process)
       .HasForeignKey(p => p.RecruitmentProcessId);
-    
-    builder.Entity<RecruitmentPhase>()
-      .HasMany(p => p.CandidateProfiles)
-      .WithOne(p => p.RecruitmentPhase)
-      .HasForeignKey(p => p.RecruitmentPhaseId);
-    
-    builder.Entity<CandidateProfile>()
-      .HasOne(c => c.JobArea)
-      .WithMany(c => c.CandidateProfiles)
-      .HasForeignKey(c => c.JobAreaId);
 
     // Apply Snake Case Naming Convention
     builder.UseSnakeCaseNamingConvention();
