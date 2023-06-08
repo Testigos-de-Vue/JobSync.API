b10d84e933db8294e1dcb925a50609027262383d
sayufu <lilifuye72@gmail.com>
2023-06-07 23:27:45 -0500

    fix: refactor recruitment process entity name

---
+++ JobSync.API/Recruitment/Domain/Services/Communication/ProcessResponse.cs
@@ -0,0 +1,15 @@
+﻿using JobSync.API.Recruitment.Domain.Models;
+using JobSync.API.Shared.Domain.Services.Communication;
+
+namespace JobSync.API.Recruitment.Domain.Services.Communication;
+
+public class ProcessResponse: BaseResponse<Process>
+{
+    public ProcessResponse(string message) : base(message)
+    {
+    }
+
+    public ProcessResponse(Process resource) : base(resource)
+    {
+    }
+}
