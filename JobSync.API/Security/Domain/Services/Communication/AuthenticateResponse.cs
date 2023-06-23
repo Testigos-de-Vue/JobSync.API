cfc00f7c5ed7735e18c2e5b47edba2e0e9a4df99
Andrés Leiva <aleiva1700@gmail.com>
2023-06-22 20:06:57 -0500

    refactor: add missing properties to authenticate response

--- JobSync.API/Security/Domain/Services/Communication/AuthenticateResponse.cs
+++ JobSync.API/Security/Domain/Services/Communication/AuthenticateResponse.cs
@@ -7,10 +7,12 @@ public class AuthenticateResponse
 {
 
   public int Id { get; set; }
-  public string Name { get; set; }
+  public string FirstName { get; set; }
   public string LastName { get; set; }
   public string Email { get; set; }
   public string ImageUrl { get; set; }
+  public string PhoneNumber { get; set; }
+  public bool IsSubscribedToNewsletter { get; set; }
   public string Token { get; set; }
 }
 
