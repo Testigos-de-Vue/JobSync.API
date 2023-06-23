5475c6177394ac174e103eed0368d37ac1cbdf4e
Andrés Leiva <aleiva1700@gmail.com>
2023-06-22 23:13:58 -0500

    feat: add jwt handler implementation

---
+++ JobSync.API/Security/Authorization/Middleware/JwtMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using JobSync.API.Security.Authorization.Handlers.Interfaces;
+using JobSync.API.Security.Authorization.Settings;
+using JobSync.API.Security.Domain.Services;
+
+namespace JobSync.API.Security.Authorization.Middleware;
+
+public class JwtMiddleware
+{
+  private readonly RequestDelegate _next;
+  private readonly AppSettings _appSettings;
+  
+  public JwtMiddleware(RequestDelegate next, AppSettings appSettings)
+  {
+    _next = next;
+    _appSettings = appSettings;
+  }
+
+  public async Task Invoke(HttpContext context, IUserService userService, IJwtHandler handler)
+  {
+    var token = context.Request.Headers["Authorization"]
+      .FirstOrDefault()?
+      .Split(" ")
+      .Last();
+
+    var userId = handler.ValidateToken(token);
+
+    if (userId != null)
+      context.Items["User"] = await userService.GetByIdAsync(userId.Value);
+
+    await _next(context);
+  }
+}
