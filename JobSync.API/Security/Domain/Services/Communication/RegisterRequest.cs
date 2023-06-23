a2846510ab3739e9645b04948189471ff0196cd6
Andrés Leiva <aleiva1700@gmail.com>
2023-06-22 20:10:59 -0500

    feat: add register and update request data transfer objects

--- JobSync.API/Security/Domain/Services/Communication/RegisterRequest.cs
+++ JobSync.API/Security/Domain/Services/Communication/RegisterRequest.cs
@@ -4,9 +4,11 @@ namespace JobSync.API.Security.Domain.Services.Communication;
 
 public class RegisterRequest
 {
-  [Required] public string Name { get; set; }
+  [Required] public string FirstName { get; set; }
   [Required] public string LastName { get; set; }
   [Required] public string Email { get; set; }
+  [Required] public string Password { get; set; }
   [Required] public string PhoneNumber { get; set; }
   [Required] public bool IsSubscribedToNewsletter { get; set; }
+  [Required] public bool isRecruiter { get; set; }
 }
