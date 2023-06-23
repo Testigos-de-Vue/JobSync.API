b148e02c6c1fb8f2312616797b658397f3ff3afc
Andrés Leiva <aleiva1700@gmail.com>
2023-06-23 12:25:10 -0500

    refactor: improve response types and better support for null properties

--- JobSync.API/Security/Domain/Services/Communication/RegisterRequest.cs
+++ JobSync.API/Security/Domain/Services/Communication/RegisterRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace JobSync.API.Security.Domain.Services.Communication;
 
@@ -7,6 +8,7 @@ public class RegisterRequest
   [Required] public string FirstName { get; set; }
   [Required] public string LastName { get; set; }
   [Required] public string Email { get; set; }
+  public string ImageUrl { get; set; } = "https://cdn-icons-png.flaticon.com/512/3237/3237472.png";
   [Required] public string Password { get; set; }
   [Required] public string PhoneNumber { get; set; }
   [Required] public bool IsSubscribedToNewsletter { get; set; }
