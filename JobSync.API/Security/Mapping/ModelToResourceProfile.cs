69235912d2346312ca25feb4f20c2dd5458f9bc6
Andrés Leiva <aleiva1700@gmail.com>
2023-06-23 13:28:28 -0500

    refactor: improve on jwt handler, mapping and app settings configuration

--- JobSync.API/Security/Mapping/ModelToResourceProfile.cs
+++ JobSync.API/Security/Mapping/ModelToResourceProfile.cs
@@ -8,11 +8,7 @@ public class ModelToResourceProfile : AutoMapper.Profile
 {
   public ModelToResourceProfile()
   {
-    CreateMap<RegisterRequest, User>();
-    CreateMap<UpdateRequest, User>().ForAllMembers(options => options.Condition((source, target, property) => {
-        if (property == null) return false;
-        return property.GetType() != typeof(string) || !string.IsNullOrEmpty((string)property);
-      })
-    );
+    CreateMap<User, AuthenticateResponse>();
+    CreateMap<User, UserResource>();
   }
 }
